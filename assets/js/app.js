24c032911e3e85b7aea9dee4ac6c79f5658f8f5f
ValeOlivares <valentinaolivares85@gmail.com>
2017-11-12 22:32:54 -0300

    cambiando boton

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,40 @@
-function add(){
-  var texto = document.getElementById("comentario");  
-  var nuevoTexto = texto.value; //rescatando el valor
-  document.getElementById("comentario").value=""; // borrar comentario después de hacer click
-//crear nuevos elementos
-  var nContenedor = document.createElement("div");
-  var nCaja= document.getElementById("caja2");
-  var parrafo = document.createElement("p");
-  var nodoText = document.createTextNode(nuevoTexto);
-  //agregar elementos ya creados
-  parrafo.appendChild(nodoText);
-  nContenedor.appendChild(parrafo);
-  nCaja.appendChild(nContenedor);
+function trello(){
+  var container = document.getElementById("container");
+  var btn= document.createElement("button");
+  var input= document.createElement("input");
+  var nodoText= document.createTextNode("Guardar");
+
+  // atributos
+  input.setAttribute("id", "valor");
+  // nodos
+  btn.appendChild(nodoText);
+  container.appendChild(btn);
+  container.appendChild(input);
+
+btn.addEventListener("click", function(){
+  var list=document.getElementById("valor").value;
+  document.getElementById("valor").value="";
+  var contiene=document.getElementById("contiene");
+  var parrafo=document.createElement("p");
+  var textNuevo= document.createTextNode(list);
+  var add=document.createElement("a");
+  add.setAttribute("href", "#");
+  var textAdd=document.createTextNode("Añadir tarjeta");
+
+
+  parrafo.appendChild(textNuevo);
+  contiene.appendChild(parrafo);
+  add.appendChild(textAdd);
+  contiene.appendChild(add);
+
+  add.addEventListener("click" , function(){
+    var textArea=document.createElement("textArea");
+    var add=document.createElement("a");
+    add.setAttribute("href", "#");
+    var textAdd=document.createTextNode("Añadir tarjeta");
+    contiene.appendChild(textArea);
+    contiene.appendChild(add);
+  })
+
+})
 }
